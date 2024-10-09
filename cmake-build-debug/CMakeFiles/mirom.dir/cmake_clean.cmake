b3a59e9e9784ef76bf53507b7de59db548e61c38
AlexTurtle <tarzan434343@gmail.com>
2024-10-09 12:31:35 +0300

    mda

---
+++ cmake-build-debug/CMakeFiles/mirom.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mirom.dir/App.cpp.o"
+  "CMakeFiles/mirom.dir/App.cpp.o.d"
+  "CMakeFiles/mirom.dir/Mirror.cpp.o"
+  "CMakeFiles/mirom.dir/Mirror.cpp.o.d"
+  "CMakeFiles/mirom.dir/main.cpp.o"
+  "CMakeFiles/mirom.dir/main.cpp.o.d"
+  "mirom"
+  "mirom.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mirom.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/mirom.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for mirom.
+# This may be replaced when dependencies are built.
