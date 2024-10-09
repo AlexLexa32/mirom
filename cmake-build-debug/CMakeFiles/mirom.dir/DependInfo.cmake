8face88bf49db95405770429e5ba9a92527f0d2d
AlexTurtle <tarzan434343@gmail.com>
2024-10-09 16:14:03 +0300

    mda

--- cmake-build-debug/CMakeFiles/mirom.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/mirom.dir/DependInfo.cmake
@@ -8,9 +8,9 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/Users/macbook/miromdwjsj/App.cpp" "CMakeFiles/mirom.dir/App.cpp.o" "gcc" "CMakeFiles/mirom.dir/App.cpp.o.d"
-  "/Users/macbook/miromdwjsj/Mirror.cpp" "CMakeFiles/mirom.dir/Mirror.cpp.o" "gcc" "CMakeFiles/mirom.dir/Mirror.cpp.o.d"
-  "/Users/macbook/miromdwjsj/main.cpp" "CMakeFiles/mirom.dir/main.cpp.o" "gcc" "CMakeFiles/mirom.dir/main.cpp.o.d"
+  "/Users/macbook/miromwdscx/App.cpp" "CMakeFiles/mirom.dir/App.cpp.o" "gcc" "CMakeFiles/mirom.dir/App.cpp.o.d"
+  "/Users/macbook/miromwdscx/Mirror.cpp" "CMakeFiles/mirom.dir/Mirror.cpp.o" "gcc" "CMakeFiles/mirom.dir/Mirror.cpp.o.d"
+  "/Users/macbook/miromwdscx/main.cpp" "CMakeFiles/mirom.dir/main.cpp.o" "gcc" "CMakeFiles/mirom.dir/main.cpp.o.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/macbook/miromdwjsj
+# Install script for directory: /Users/macbook/miromwdscx
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/macbook/miromdwjsj/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/macbook/miromwdscx/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
