9e7d2f556aa0e2a18f1942394c4209a11a991560
AlexTurtle <tarzan434343@gmail.com>
2024-10-16 11:27:22 +0300

    mda

--- cmake-build-debug/CMakeFiles/mirom.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/mirom.dir/DependInfo.cmake
@@ -8,17 +8,13 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/Users/noobmaster_2007/programming/pracs/miromfd/App.cpp" "CMakeFiles/mirom.dir/App.cpp.o" "gcc" "CMakeFiles/mirom.dir/App.cpp.o.d"
-  "/Users/noobmaster_2007/programming/pracs/miromfd/Mirror.cpp" "CMakeFiles/mirom.dir/Mirror.cpp.o" "gcc" "CMakeFiles/mirom.dir/Mirror.cpp.o.d"
-  "/Users/noobmaster_2007/programming/pracs/miromfd/main.cpp" "CMakeFiles/mirom.dir/main.cpp.o" "gcc" "CMakeFiles/mirom.dir/main.cpp.o.d"
+  "/Users/macbook/miromfe/App.cpp" "CMakeFiles/mirom.dir/App.cpp.o" "gcc" "CMakeFiles/mirom.dir/App.cpp.o.d"
+  "/Users/macbook/miromfe/Mirror.cpp" "CMakeFiles/mirom.dir/Mirror.cpp.o" "gcc" "CMakeFiles/mirom.dir/Mirror.cpp.o.d"
+  "/Users/macbook/miromfe/main.cpp" "CMakeFiles/mirom.dir/main.cpp.o" "gcc" "CMakeFiles/mirom.dir/main.cpp.o.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
 set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
-  )
-
-# Targets to which this target links which contain Fortran sources.
-set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
